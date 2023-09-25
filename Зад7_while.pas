83a894ceff396d444dd571e3db36341bc7a22547
Kr0nys <144237820+Kr0nys@users.noreply.github.com>
2023-09-25 21:24:34 +0300

    Add files via upload

---
+++ Зад7_while.pas
@@ -0,0 +1,14 @@
+﻿Program Zad7;
+var x,j:real; n,m:integer;
+begin
+ write('Введите отрезок: ');
+ read(n,m);
+ write('Введите шаг: ');
+ read(j);
+
+    x:=n;
+    while x<=m do begin
+      writeln('y = ',3*sqr(x)-exp(ln(2)*x));
+      x:=x+j;
+    end;
+end.
