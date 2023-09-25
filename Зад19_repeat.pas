5f3e12d3c31498f76579d82f32b1fa90f461faa6
Kr0nys <144237820+Kr0nys@users.noreply.github.com>
2023-09-25 22:41:58 +0300

    Update Зад19_repeat.pas

--- Зад19_repeat.pas
+++ Зад19_repeat.pas
@@ -1,13 +1,14 @@
-﻿Program Zad19;
+Program Zad19;
 var a,b,c: integer;
 begin
   write('Введите число: ');
    read(a);
    b := 0;
-   while a > 0 do begin
+   repeat  begin
       c := a mod 10;
       b := b * 10 + c;
       a := a div 10;
     end;
+    until a=0;
    writeln(b);
 end.
